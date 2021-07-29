ff72f28ff2195418e6b0832aaac7a3fb2fc78466
Maxim <shafeevms@gmail.com>
2021-07-30 00:48:28 +0300

    swiper added

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,22 @@ const css = () => {
   .pipe(dest('./dist'));
 };
 
+// const scripts = () => {
+//   return src([
+//     './src/vendor/swiper.js',
+//     'src/*.js'
+//   ])
+//     .pipe(concat('app.js'))
+//     .pipe(sourcemaps.init())
+//     .pipe(babel({
+//       presets: ['@babel/env']
+//     }))
+//     .pipe(uglify().on('error', notify.onError()))
+//     .pipe(sourcemaps.write())
+//     .pipe(dest('dist'))
+//     .pipe(browserSync.stream())
+// };
+
 const html = () => {
   return src('./src/*.html')
     .pipe(dest('./dist'));
@@ -32,8 +48,27 @@ const resources = () => {
     .pipe(dest('./dist/public'))
 };
 
+const vendorJS = () => {
+  const modules = [
+    './node_modules/swiper/swiper-bundle.min.js',
+    './node_modules/swiper/swiper-bundle.min.js.map',
+  ];
+
+  return src(modules)
+    .pipe(dest('./dist/public/js'));
+};
+
+const vendorCSS = () => {
+  const modules = [
+    'node_modules/swiper/swiper-bundle.min.css',
+  ];
+
+  return src(modules)
+    .pipe(dest('./dist/public/css'));
+};
+
 watch('./src/styles/*.scss', css);
 watch('./src/public', resources);
 watch('./src/*.html', html);
 
-exports.default = series(resources, css, html, watchFiles);
+exports.default = series(resources, vendorCSS, css, html, vendorJS, watchFiles);
